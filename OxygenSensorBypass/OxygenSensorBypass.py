175df238ee381a437c7dfc4fc1524419fc712b5a
ShaneCalado <43459552+ShaneCalado@users.noreply.github.com>
2020-06-09 13:24:28 -0700

    Added basic IO function

--- OxygenSensorBypass/OxygenSensorBypass.py
+++ OxygenSensorBypass/OxygenSensorBypass.py
@@ -1,1 +1,72 @@
+# This program runs on a Raspberry Pi 3.
+# It reads the output signal of a downstream O2 sensor and sends a modified value back to the ECU.
+# This can be used to eliminate a check-engine-light on a malfunctioning sensor, or modify the
+# sensor's output to influence performance and fuel economy
 
+# Import Raspberry PI GPIO module
+import RPi.GPIO as GPIO
+
+
+global modifiedMode
+global voltageMod
+
+# 0 > Copy input voltage to output pin
+# 1 > Modify inout voltage by set amount (voltageMod) WIP
+# 2 > Modify voltage by (voltageMod) percent #WIP
+modifiedMode = 0
+voltageMod = 0
+
+# Initialize GPIO interface
+GPIO.setmode(GPIO.BOARD)
+
+# Reset all pins to original state
+GPIO.cleanup()
+
+# Define IO pins
+# pin 5, is input
+GPIO.setup(5, GPIO.IN, pull_up_daown=GPIO.PUD_DOWN)
+# pin 6 is output
+GPIO.setup(6, GPIO.OUT)
+
+# Function to read the input voltage of pin 5, and output it to pin 6
+def copyInputSignal():
+    # Read inout from pin 5
+    inputPinSignal = GPIO.input(5)
+    # Output to pin 6
+    GPIO.output(6, inputPinSignal)
+
+# Function to read the input voltage of pin 5, modify it my a fixed amount, and output it to pin 6
+def modifyInputSignalFixed(int):
+    # Read inout from pin 5
+    inputPinSignal = GPIO.input(5)
+    newInputSignal = inputPinSignal + voltageMod
+    # if the new voltage is within a safe range, output it, otherwise output unmodified voltage
+    if newInputPinSignal <= GPIO.HIGH and newInputPinSignal >= GPIO.LOW:
+        GPIO.output(6, newInputPinSignal)
+    else:
+        GPIO.output(6, inputPinSignal)
+
+# Function to read the input voltage of pin 5, modify it my a percent, and output it to pin 6
+def modifyInputSignalPercent(int):
+    # Read inout from pin 5
+    inputPinSignal = GPIO.input(5)
+    newInputSignal = inputPinSignal * (voltageMod / 100)
+    # if the new voltage is within a safe range, output it, otherwise output unmodified voltage
+    if newInputPinSignal <= GPIO.HIGH and newInputPinSignal >= GPIO.LOW:
+        GPIO.output(6, newInputPinSignal)
+    else:
+        GPIO.output(6, inputPinSignal)
+
+# While receiving input from pin 5 (engine is running, O2 sensor sending signal)
+while GPIO.input(5) >= GPIO.HIGH:
+    if modifiedMode == 0:
+        # Copy input to output
+        copyInputSignal()
+    elif modifiedMode == 1:
+        # modified signal
+        modifyInputSignalFixed(voltageMod)
+    elif modifiedMode == 2:
+        # modified signal 
+        modifyInputSignalPercent(voltageMod)
+    else:
+        # invalid mode
